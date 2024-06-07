64228ac35b19c984222aebbb432b4f86ceee62de
Carla Buzatto <vargasbuzatto@gmail.com>
2024-06-07 17:24:04 -0300

    continuando as telas do médico

--- src/pages/RegistroPacientes.js
+++ src/pages/RegistroPacientes.js
@@ -1,5 +1,12 @@
 import React, { useState } from 'react';
 import './RegistroPacientes.css';
+import medico from './medico.png';
+import lupa from './lupa.png';
+import oxigenio from './oxigenio.png';
+import coracao from './coracao.png';
+import termometro from './termometro.png';
+import pressao from './pressao.png'; 
+import carolina from './carolina.png';
 
 const RegistroPacientes = () => {
   const [searchTerm, setSearchTerm] = useState('');
@@ -10,15 +17,43 @@ const RegistroPacientes = () => {
 
   return (
     <div className="registro-pacientes">
-      <h1>REGISTRO DE PACIENTES</h1>
-      <form>
-        <input
-          type="search"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          placeholder="Pesquisar"
-        /> <button type="button" onClick={handleSearch}>PESQUISAR</button>
+      <div className="parent-container">
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <img src={medico} alt="Médico" /> 
+          <h2>Dr. Adriano Lopes</h2> 
+        </div>
+        <div className="registroP">
+          <h1>REGISTRO DE PACIENTES</h1> 
+        </div>
+      </div>
+      <form onSubmit={handleSearch}>
+        <div className="search-container">
+          <input
+            type="search"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder="Pesquisar"
+          />
+          <img src={lupa} alt="Ícone de lupa" className="search-icon" />
+        </div>
       </form>
+      <div className="paciente-card">
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <img src={carolina} alt="Paciente" className="paciente-img" />
+        <div className="paciente-info">
+          <h4>Carolina Lopes</h4>
+          <div className="paciente-status">
+            <div className="status-bar" style={{ width: "40px" }}>
+              <span className="status-percentage">45%</span>
+            </div>
+            </div>
+            <img src={oxigenio} alt="Pulmão" className="status-icon" />
+            <img src={coracao} alt="Coração" className="status-icon" />
+            <img src={termometro} alt="Termômetro" className="status-icon" />
+            <img src={pressao} alt="Pressão" className="status-icon" />
+            </div>
+        </div>
+      </div>
     </div>
   );
 };
